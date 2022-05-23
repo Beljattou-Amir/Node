1d220bd858c0af6b751df4cee812ebce05306a06
beljattou amir <abeljattou@gmail.com>
2022-05-23 15:24:05 +0200

    creating routes and testing get and post methods

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require ('express')
 const app = express()
 
-app.use((req, res, next) => {
-    res.status(200).json({
-        message:'it works!!!!'
-    })
-})
+const taskRoutes = require('./api/routes/task')
 
+// app.use((req, res, next) => {
+//     res.status(200).json({
+//         message:'it works!!!!'
+//     })
+// })
+
+app.use('/task', taskRoutes)
 
 module.exports = app;
