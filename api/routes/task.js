b85c23b32e6ef971c9b7aa0a42751d6f904421c1
beljattou amir <abeljattou@gmail.com>
2022-05-23 15:30:51 +0200

    adding some comments to the code

--- api/routes/task.js
+++ api/routes/task.js
@@ -1,17 +1,22 @@
 const express = require('express')
 const router = express.Router()
 
-
+//GET method
 router.get('/', (req, res, next) => {
     res.status(200).json({
         message:'Handling GET requests to /task'
     })
 })
-
+//POST method
 router.post('/', (req, res, next) => {
     res.status(200).json({
         message:'Handling POST requests to /task'
     })
+})
+
+router.get('/:taskId', (req, res, next) => {
+    const id = req.params.taskId
+    
 })
 
 module.exports = router;
